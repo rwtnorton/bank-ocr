c11e97bc09a125133e2b4471e4273b1d92737c74
Richard W. Norton <rwtnorton@gmail.com>
2019-01-08 22:57:58 -0800

    added fn parse-ocr-digit

---
+++ src/bank_ocr/parser.clj
@@ -0,0 +1,20 @@
+(ns bank-ocr.parser
+  (:require [bank-ocr.ocr-digit :as ocr-digit]))
+
+(defn parse-ocr-digit
+  [ocr-digit]
+  (condp = ocr-digit
+    ocr-digit/zero 0
+    ocr-digit/one 1
+    ocr-digit/two 2
+    ocr-digit/three 3
+    ocr-digit/four 4
+    ocr-digit/five 5
+    ocr-digit/six 6
+    ocr-digit/seven 7
+    ocr-digit/eight 8
+    ocr-digit/nine 9
+    ;; else
+    :dunno))
+
+
---
+++ test/bank_ocr/parse_test.clj
@@ -0,0 +1,21 @@
+(ns bank-ocr.parser-test
+  (:require [clojure.test :refer :all]
+            [bank-ocr.parser :as parser]
+            [bank-ocr.ocr-digit :as ocr-digit]))
+
+(deftest parse-ocr-digit
+  (are [v expected] (= (parser/parse-ocr-digit v) expected)
+    nil :dunno
+    [] :dunno
+    42 :dunno
+    "one" :dunno
+    ocr-digit/zero 0
+    ocr-digit/one 1
+    ocr-digit/two 2
+    ocr-digit/three 3
+    ocr-digit/four 4
+    ocr-digit/five 5
+    ocr-digit/six 6
+    ocr-digit/seven 7
+    ocr-digit/eight 8
+    ocr-digit/nine 9))
