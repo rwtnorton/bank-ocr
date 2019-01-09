45372e3f36e74c05f79d87fc2eb52819397cc6ae
Richard W. Norton <rwtnorton@gmail.com>
2019-01-08 19:30:19 -0800

    ditch core namespace

--- src/bank_ocr/core.clj
+++
@@ -1,6 +0,0 @@
-(ns bank-ocr.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/bank_ocr/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns bank-ocr.core-test
-  (:require [clojure.test :refer :all]
-            [bank-ocr.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
