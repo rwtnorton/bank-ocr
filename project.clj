b6e1476d290089a9851147e606fe3992c261ae67
Richard W. Norton <rwtnorton@gmail.com>
2019-01-08 19:34:03 -0800

    oh yeah, v1.10 is a thing now

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject bank-ocr "0.1.0-SNAPSHOT"
   :description "Scans seven-segment display numbers and converts into their arabic numeral counterparts"
-  :url ""
+  :url "https://github.com/rwtnorton/bank-ocr"
   :license {:name "Apache License, version 2.0"
-            :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns bank-ocr.main}
   :main bank-ocr.main)
